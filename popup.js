afd880804ef862cebd9af297fa070357c5394d96
Dongkeun.YOON <31213371+Bronzeroot@users.noreply.github.com>
2023-04-12 15:56:21 +0900

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  var tab = tabs[0];
-  chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-    // Do something with the screenshot URL
-  });
-});
